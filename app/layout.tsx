a5372f71fe1aba530cbde8eed7dc4c962502b03f
andbudha <andreibartov@gmail.com>
2024-09-27 13:33:52 +0200

    theme-provider comp added/app wrapped with ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Plus_Jakarta_Sans } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
+import { ThemeProvider } from 'next-themes';
 
 const fontSans = Plus_Jakarta_Sans({
   subsets: ['latin'],
@@ -26,7 +27,9 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
