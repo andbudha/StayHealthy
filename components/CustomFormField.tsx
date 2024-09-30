0e6c996bbd89bfaa40314fb9c79941ed31d835d2
andbudha <andreibartov@gmail.com>
2024-09-30 16:26:36 +0200

    SubmitButton comp created/func-ties&styles added

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -39,7 +39,7 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
         <div className="flex rounded-md border border-dark-500 bg-dark-400">
           {props.iconSrc && (
             <Image
-              src={iconSrc ? iconSrc : ''}
+              src={iconSrc ?? ''}
               height={24}
               width={24}
               alt={iconAlt || 'icon'}
