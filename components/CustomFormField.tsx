c6b56515a53ec5cf568dbd8e98e1e05be9220c05
andbudha <andreibartov@gmail.com>
2024-09-30 13:47:08 +0200

    CustomFormField comp created&styled

---
+++ components/CustomFormField.tsx
@@ -0,0 +1,97 @@
+'use client';
+import 'react-phone-number-input/style.css';
+import PhoneInput, { type Value } from 'react-phone-number-input';
+import { E164Number } from 'libphonenumber-js/core';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Control } from 'react-hook-form';
+import { FormFieldType } from './forms/PatientForm';
+import Image from 'next/image';
+
+type CustomProps = {
+  control: Control<any>;
+  fieldType: FormFieldType;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconAlt?: string;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  renderSkeleton?: (filed: any) => React.ReactNode;
+};
+
+const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  const { iconSrc, iconAlt, fieldType, placeholder } = props;
+  switch (fieldType) {
+    case FormFieldType.INPUT:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-400">
+          {props.iconSrc && (
+            <Image
+              src={iconSrc ? iconSrc : ''}
+              height={24}
+              width={24}
+              alt={iconAlt || 'icon'}
+              className="ml-2"
+            />
+          )}
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              {...field}
+              className="shad-input border-0"
+            />
+          </FormControl>
+        </div>
+      );
+    case FormFieldType.PHONE_INPUT:
+      return (
+        <FormControl>
+          <PhoneInput
+            defaultCountry="DE"
+            placeholder={placeholder}
+            international
+            withCountryCallingCode
+            value={field.value as string | E164Number | undefined}
+            className="input-phone"
+            onChange={field.onChange}
+          />
+        </FormControl>
+      );
+
+    default:
+      break;
+  }
+};
+
+const CustomFormField = (props: CustomProps) => {
+  const { control, fieldType, label, name } = props;
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex-1">
+          {fieldType !== FormFieldType.CHECKBOX && label && (
+            <FormLabel>{label}</FormLabel>
+          )}
+          <RenderField field={field} props={props} />
+          <FormMessage className="shad-err" />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default CustomFormField;
