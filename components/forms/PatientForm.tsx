3a447924a25cd388b5aaf804fc01c509bca34b08
andbudha <andreibartov@gmail.com>
2024-10-02 15:59:17 +0200

    registering new user func-ty implemented

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -8,6 +8,8 @@ import CustomFormField from '../CustomFormField';
 import SubmitButton from '../SubmitButton';
 import { useState } from 'react';
 import { UserFormValidation } from '@/lib/validation';
+import { createUser } from '@/lib/actions/patient.actions';
+import { useRouter } from 'next/navigation';
 
 export enum FormFieldType {
   INPUT = 'input',
@@ -21,6 +23,7 @@ export enum FormFieldType {
 
 const PatientForm = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const router = useRouter();
 
   const form = useForm<z.infer<typeof UserFormValidation>>({
     resolver: zodResolver(UserFormValidation),
@@ -31,9 +34,25 @@ const PatientForm = () => {
     },
   });
 
-  function onSubmit(values: z.infer<typeof UserFormValidation>) {
-    console.log(values);
-  }
+  const onSubmit = async ({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) => {
+    setIsLoading(true);
+    try {
+      const userData = { name, email, phone };
+      const newUser = await createUser(userData);
+      if (newUser) {
+        router.push(`/patients/${newUser.$id}/register`);
+        console.log(newUser);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <div>
       <Form {...form}>
