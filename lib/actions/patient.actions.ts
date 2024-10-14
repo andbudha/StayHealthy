d501db341fc4e7c19a0358d3af30177c93ee6279
andbudha <andreibartov@gmail.com>
2024-10-14 12:18:16 +0200

    registerPatient action added

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,5 +1,7 @@
+'use server';
 import { ID, Query } from 'node-appwrite';
-import { users } from '../appwrite.config';
+import { InputFile } from 'node-appwrite/file';
+import { databases, storage, users } from '../appwrite.config';
 import { parseStringify } from '../utils';
 
 export const createUser = async (user: CreateUserParams) => {
@@ -29,5 +31,38 @@ export const getUser = async (userID: string) => {
     return parseStringify(user);
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const registerPatient = async ({
+  identificationDocument,
+  ...patient
+}: RegisterUserParams) => {
+  try {
+    let file;
+    if (identificationDocument) {
+      const inputFile = InputFile.fromBuffer(
+        identificationDocument.get('blobFile') as Blob,
+        identificationDocument.get('fileName') as string
+      );
+      file = await storage.createFile(
+        process.env.NEXT_PUBLIC_BUCKET_ID!,
+        ID.unique(),
+        inputFile
+      );
+    }
+    const newPatient = await databases.createDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_PATIENTS_COLLECTION_ID!,
+      ID.unique(),
+      {
+        identificationDocumentId: file?.$id || null,
+        identificationDocumentUrl: `${process.env.NEXT_PUBLIC_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_BUCKET_ID}/files/${file?.$id}/view?project=${process.env.NEXT_PUBLIC_PROJECT_ID}`,
+        ...patient,
+      }
+    );
+    return parseStringify(newPatient);
+  } catch (error) {
+    console.log('Registering new patient:::', error);
   }
 };
