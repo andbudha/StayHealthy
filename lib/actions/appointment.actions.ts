54ff6196ab97985299f620a62ca399c296ede5b4
andbudha <andreibartov@gmail.com>
2024-10-23 13:11:11 +0200

    sending sms func-ty added

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,7 +1,7 @@
 'use server';
 import { ID, Query } from 'node-appwrite';
-import { databases } from '../appwrite.config';
-import { parseStringify } from '../utils';
+import { databases, messaging } from '../appwrite.config';
+import { formatDateTime, parseStringify } from '../utils';
 import { Appointment } from '@/types/appwrite.types';
 import { revalidatePath } from 'next/cache';
 
@@ -38,8 +38,8 @@ export const getRecentAppontmentList = async () => {
   try {
     const appointments = await databases.listDocuments(
       process.env.NEXT_PUBLIC_DATABASE_ID!,
-      process.env.NEXT_PUBLIC_APPOINTMENTS_COLLECTION_ID!
-      // [Query.orderDesc('$createdAt')]
+      process.env.NEXT_PUBLIC_APPOINTMENTS_COLLECTION_ID!,
+      [Query.orderDesc('$createdAt')]
     );
     const initialCounts = {
       scheduledCount: 0,
@@ -83,6 +83,8 @@ export const updateAppointment = async ({
   appointment,
   type,
 }: UpdateAppointmentParams) => {
+  console.log('APPOINTMENT DETAILS:::', appointment);
+
   try {
     const updatedAppointment = await databases.updateDocument(
       process.env.NEXT_PUBLIC_DATABASE_ID!,
@@ -93,11 +95,34 @@ export const updateAppointment = async ({
     if (!updatedAppointment) {
       throw new Error('Appointment not found!');
     }
+    const smsMessage = `Hi, it's StayHealthy. ${
+      type === 'schedule'
+        ? `Your appointment has been scheduled for ${
+            formatDateTime(appointment.schedule!).dateTime
+          } with Dr.${appointment.primaryPhysician}`
+        : `We regret to inform you that your apointment has been cancelled!. 
+        Reason: ${appointment.cancellationReason!}`
+    }
+  `;
 
+    await sendSMSNotification(userId, smsMessage);
     //SMS notification
     revalidatePath('/admin');
     return parseStringify(updatedAppointment);
   } catch (error) {
     console.log('UPDATING APPOINTMENT ERROR:::', error);
+  }
+};
+
+export const sendSMSNotification = async (userId: string, content: string) => {
+  try {
+    const message = await messaging.createSms(
+      ID.unique(),
+      content,
+      [],
+      [userId]
+    );
+  } catch (error) {
+    console.log('SENDING SMS TOTIFICATION ERROR:::', error);
   }
 };
